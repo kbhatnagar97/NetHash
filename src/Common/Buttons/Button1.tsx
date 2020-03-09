ae441104b34681a08d51f7930320d787c846c713
Kshitij Bhatnagar <kshitij.bhatnagar@hashedin.com>
2020-03-09 12:24:01 +0530

    Button
    Button component added
    CSS applied

--- src/Common/Buttons/Button1.tsx
+++ src/Common/Buttons/Button1.tsx
@@ -4,7 +4,10 @@ import './Button1.css';
 export const Button = () => {
     return (
         <div className="Button1">
-            
+            <link href='https://fonts.googleapis.com/css?family=Fredoka One' rel='stylesheet'/>
+            <button className="Button">
+                >
+            </button>
         </div>
     );
 }
